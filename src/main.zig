cc524a28d60f280acfe9086a8592eb0c9b13bad6
tacheraSasi <tacherasasi@gmail.com>
2026-02-16 10:25:38 +0300

    Refactor main function: improve file handling and error messaging

--- src/main.zig
+++ src/main.zig
@@ -1,29 +1,39 @@
 const std = @import("std");
+const fs = std.fs;
 const ccat = @import("ccat");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
+    const cwd = fs.cwd();
+    const file_open_flags = fs.File.OpenFlags{ .mode = .read_only };
     const args = std.process.argsAlloc(allocator) catch {
-        std.debug.print("Failed to allocate memory for arguments.\n", .{});
+        printer("Failed to allocate memory for arguments.\n", .{});
         return;
     };
     defer std.process.argsFree(allocator, args);
 
     const filePath = if (args.len > 1) args[1] else unreachable;
 
-    const file = try std.fs.cwd().openFile(filePath, .{ .mode = .read_only });
+    const file = try cwd.openFile(filePath, file_open_flags);
     defer file.close();
 
-    const file_content = try file.reader(std.math.maxInt(usize)).readToEndAlloc(allocator, std.math.maxInt(usize));
-    defer allocator.free(file_content);
+    const buffer_size = std.math.maxInt(usize);
+    const buffer = allocator.alloc(u8, buffer_size) catch {
+        printer("Failed to allocate memory for file buffer.\n", .{});
+        return;
+    };
 
-    try printer(file_content);
+    const bytesRead = try file.read(buffer);
+    defer allocator.free(buffer);
+
+    printer("Read {} bytes from file '{}'.\n", .{bytesRead, filePath});
+
 }
 
-fn printer(message: []const u8) !void {
+fn printer(fmt: []const u8, arg: anytype) !void {
     var stdout_buf: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
     const stdout: *std.io.Writer = &stdout_writer.interface;
-    try stdout.writeAll(message);
+    try stdout.writeAll(fmt, .{arg});
     try stdout.flush();
 }
