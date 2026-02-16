4ee94cb04c0407daf96b813ea989f3185782d340
tacheraSasi <tacherasasi@gmail.com>
2026-02-16 10:00:58 +0300

    Refactor main function: remove debug print and tests, streamline argument allocation

--- src/main.zig
+++ src/main.zig
@@ -2,26 +2,11 @@ const std = @import("std");
 const ccat = @import("ccat");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try ccat.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    const allocator = std.heap.page_allocator;
+    const args =  std.process.argsAlloc(allocator) catch {
+        std.debug.print("Failed to allocate memory for arguments.\n", .{});
+        return;
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    defer std.process.argsFree(allocator, args);
+    
 }
